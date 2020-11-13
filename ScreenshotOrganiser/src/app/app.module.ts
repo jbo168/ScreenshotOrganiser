5869a70ca5b0f1c3aa16e4bc92d8b25b9101ede3
John Long <johnlong@amcsgroup.com>
2020-11-13 15:40:32 +0000

    Added simple draggable element

--- ScreenshotOrganiser/src/app/app.module.ts
+++ ScreenshotOrganiser/src/app/app.module.ts
@@ -1,18 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
+  imports: [BrowserModule, DragDropModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
